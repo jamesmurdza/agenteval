381b4f195a167a86743cfcfe7f2813019ea855d4
James Murdza <james@jamesmurdza.com>
2023-06-23 18:19:03 -0700

    Get the test results using NodeJS.

--- evals.js
+++ evals.js
@@ -1,15 +1,34 @@
-const cypress = require('cypress');
+const cypress = require("cypress");
 
 async function runCypressTests() {
   try {
-    await cypress.run({
+    const results = await cypress.run({
       headless: true,
-      video: false,
-      spec: 'cypress/e2e/counter_spec.cy.js'
+      config: {
+        video: false,
+      },
+      spec: "cypress/e2e/counter_spec.cy.js",
+      reporter: "junit",
+      quiet: true,
     });
+
+    return results;
   } catch (error) {
-    console.error('Cypress tests failed:', error);
+    console.error("Cypress tests failed:", error);
+    throw error;
   }
 }
 
-runCypressTests();
+runCypressTests()
+  .then((results) => {
+    // Handle the test results here
+    console.log("Cypress tests completed successfully:");
+    console.log("Total tests:", results.totalTests);
+    console.log("Total passed:", results.totalPassed);
+    console.log("Total failed:", results.totalFailed);
+    console.log("Duration:", results.totalDuration);
+  })
+  .catch((error) => {
+    // Handle any errors that occurred during test execution
+    console.error("Cypress tests failed:", error);
+  });
